495e93be1880f8d8fa300b716117c4ad2f4eec3a
devyagnesh <con.boringmacho@gmail.com>
2026-06-10 23:11:18 +0530

    feat: add employee portal self attendance

--- resources/views/components/layouts/employee.blade.php
+++ resources/views/components/layouts/employee.blade.php
@@ -113,6 +113,7 @@
                                 </a>
                             </li>
                         @endcan
+                        @can('hr.leave.apply')
                             <li class="slide">
                                 <a href="{{ route('employee.leave.index') }}"
                                     class="side-menu__item {{ request()->routeIs('employee.leave.*') ? 'active' : '' }}">
